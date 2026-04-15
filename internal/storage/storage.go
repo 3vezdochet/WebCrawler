c4f13ac92ad20d686b8c6de5b938717d9b6ad51e
3vezdochet <dan.dms@yandex.ru>
2026-04-15 14:07:05 +0300

    feat(storage): implement thread-secure URL storage

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,1 +1,26 @@
 package storage
+
+import "sync"
+
+type URLStorage struct {
+	mu   sync.Mutex
+	data map[string]struct{}
+}
+
+func NewURLStorage() *URLStorage {
+	return &URLStorage{
+		data: make(map[string]struct{}),
+	}
+}
+
+func (u *URLStorage) Add(url string) bool {
+	u.mu.Lock()
+	defer u.mu.Unlock()
+
+	if _, exists := u.data[url]; exists {
+		return false
+	}
+
+	u.data[url] = struct{}{}
+	return true
+}
