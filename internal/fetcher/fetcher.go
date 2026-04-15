0fdd5d041b91121f9ec2a9ef08b4e467925edda5
3vezdochet <dan.dms@yandex.ru>
2026-04-15 14:09:42 +0300

    feat(fetcher): implement HTTP client with timeouts and correct body closure

--- internal/fetcher/fetcher.go
+++ internal/fetcher/fetcher.go
@@ -1,1 +1,23 @@
 package fetcher
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+func Fetch(targetURL string) (io.ReadCloser, error) {
+	client := &http.Client{Timeout: 5 * time.Second}
+
+	resp, err := client.Get(targetURL)
+	if err != nil {
+		return nil, fmt.Errorf("network error: %w", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("bad status code: %w", resp.StatusCode)
+	}
+
+	return resp.Body, nil
+}
